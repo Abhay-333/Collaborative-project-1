3e1dd896e84c6a89ea7bb90976b828446d570421
Abhay <abhayd0331@gmail.com>
2024-06-22 20:21:14 +0530

    Marquee animation done imgs remaining

--- Project 1/script.js
+++ Project 1/script.js
@@ -1,1 +1,20 @@
+gsap.to('.moving-left',{
+    xPercent: -100,
+    duration:15,
+    repeat:-1,
+    ease:"linear",
+})
 
+gsap.to('.moving-right',{
+    xPercent: 100,
+    duration:15,
+    repeat:-1,
+    ease:"linear",
+})
+
+// gsap.to('.right',{
+//     x:200,
+//     duration:4,
+//     repeat:Infinity,
+//     ease:"linear",
+// })
